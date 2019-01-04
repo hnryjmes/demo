f2515c9ab5a0959c0b63326c578ccd1ae4beb456
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-04 12:01:59 +0000

    2D arrays

--- src/demo.java
+++ src/demo.java
@@ -1,7 +1,15 @@
+import java.util.Arrays;
+
 public class demo {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(String.valueOf(i));
+        int count = 0;
+        int[][] myArr = new int[3][5];
+        for (int i = 0; i < myArr.length; i++) {
+            for (int j = 0; j < myArr[i].length; j++) {
+                myArr[i][j] = count;
+                count++;
+            }
+            System.out.println(Arrays.toString(myArr[i]));
         }
     }
 }
