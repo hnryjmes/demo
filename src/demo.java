18ce40d317518ca186a4b868eb3501485736786a
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-04 11:27:03 +0000

    Print numbers 0 to 9

--- src/demo.java
+++ src/demo.java
@@ -1,7 +1,7 @@
 public class demo {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("Hello");
+            System.out.println(String.valueOf(i));
         }
     }
 }
