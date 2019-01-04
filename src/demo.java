ce2c1f53818cf89f80bcb6c7c60f9cf7ff5eb5d2
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-04 13:10:25 +0000

    ArrayList

--- src/demo.java
+++ src/demo.java
@@ -1,15 +1,11 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class demo {
     public static void main(String[] args) {
-        int count = 0;
-        int[][] myArr = new int[3][5];
-        for (int i = 0; i < myArr.length; i++) {
-            for (int j = 0; j < myArr[i].length; j++) {
-                myArr[i][j] = count;
-                count++;
-            }
-            System.out.println(Arrays.toString(myArr[i]));
-        }
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Henry");
+        String s = names.get(0);
+        System.out.println(s);
     }
 }
