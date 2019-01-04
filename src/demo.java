9aa0dfcb2cb2284134a395cd23cc5311ef002be8
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-04 13:17:26 +0000

    ArrayList

--- src/demo.java
+++ src/demo.java
@@ -1,11 +1,9 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class demo {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<String>();
+        ArrayList<String> names = new ArrayList<>();
         names.add("Henry");
-        String s = names.get(0);
-        System.out.println(s);
+        System.out.println(names.get(0));
     }
 }
